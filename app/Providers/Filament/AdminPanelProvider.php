900f2d3df3ee5db179c6bdf355081aba8b42e276
mtriza <muhamadtaufiqriza@unida.gontor.ac.id>
2026-01-17 00:09:03 +0800

    Revert to default Filament login - custom login causing issues

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login(\App\Filament\Pages\Auth\Login::class)
+            ->login()
             ->brandName($brandName)
             ->colors([
                 'primary' => Color::Emerald,
